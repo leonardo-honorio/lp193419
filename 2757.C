ad696c7fdba2d0da685c07a3c6df897012ed1541
leonardo-honorio <leonardo.honorio@unifesp.br>
2026-04-21 15:10:36 -0300

    Update 2757.C

--- 2757.C
+++ 2757.C
@@ -1,3 +1,13 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, Turma IB, 2026S1
+Nome        : Leonardo Rissato Honório
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/2757
+Data        : 21/04/2026
+Objetivo    : Criar variáveis, armazenar três valores e apresentar de diferentes maneiras
+Aprendizado : Diferentes maneiras de apresentar os valores armazenados
+-------------------------------------------------------------------------- */
+
 #include <stdio.h>
 
 int main()
