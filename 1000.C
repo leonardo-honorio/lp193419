83139b429f5388364612a86d36d4a145e99fc820
leonardo-honorio <leonardo.honorio@unifesp.br>
2026-04-21 14:53:35 -0300

    Update 1000.C

--- 1000.C
+++ 1000.C
@@ -3,7 +3,7 @@ Disciplina  : Lógica de Programação, Turma IB, 2026S1
 Nome        : Leonardo Rissato Honório
 Linguagem   : C
 Problema    : https://judge.beecrowd.com/pt/problems/view/1000
-Data        : DD/04/2026
+Data        : 21/04/2026
 Objetivo    : Imprimir a frase "Hello World"
 Aprendizado : Imprimir frases
 -------------------------------------------------------------------------- */
