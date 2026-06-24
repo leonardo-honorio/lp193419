c0c1e7d534f0fe1e1a07bb28e252c19357afbb4d
leonardo-honorio <leonardo.honorio@unifesp.br>
2026-06-24 01:42:45 -0300

    Create 1435.cc

--- 1435.cc
+++ 1435.cc
@@ -1,7 +1,7 @@
 /* --------------------------------------------------------------------------
 Disciplina  : Lógica de Programação, Turma IB, 2026S1
 Nome        : Leonardo Rissato Honório
-Linguagem   : C
+Linguagem   : C++
 Problema    : https://judge.beecrowd.com/pt/problems/view/1435
 Data        : 23/06/2026
 Objetivo    : Construir uma matriz quadrada
