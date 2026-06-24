2aba1bf826c044e96dc9123e6c52bdf21a91e995
leonardo-honorio <leonardo.honorio@unifesp.br>
2026-06-24 00:06:00 -0300

    Create 1435.cc

---
+++ 1435.cc
@@ -0,0 +1,49 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, Turma IB, 2026S1
+Nome        : Leonardo Rissato Honório
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1435
+Data        : 23/06/2026
+Objetivo    : Construir uma matriz quadrada
+Aprendizado : Praticar matrizes bidimensionais, laços aninhados
+-------------------------------------------------------------------------- */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int N;
+
+    while (cin >> N && N != 0) {
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+
+                int valor = i;
+
+                if (j < valor)
+                    valor = j;
+
+                if (N - 1 - i < valor)
+                    valor = N - 1 - i;
+
+                if (N - 1 - j < valor)
+                    valor = N - 1 - j;
+
+                valor++;
+
+                if (j == 0)
+                    printf("%3d", valor);
+                else
+                    printf(" %3d", valor);
+            }
+
+            printf("\n");
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
